967ebaf81096e8e40c1de68d3f0aa6d08e8ee744
Rashi Goyal <rashiwork17@gmail.com>
2025-08-07 15:17:30 +0530

    chore: changed uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const Environment : any = {
-    URI: "https://localhost:7191/api/",
-    // URI: "https://rashiwebapp-djcxf6fjdscte5hq.canadacentral-01.azurewebsites.net/api/",
+    // URI: "https://localhost:7191/api/",
+    URI: "https://rashiwebapp-djcxf6fjdscte5hq.canadacentral-01.azurewebsites.net/api/",
 }
